ba808b63bd39236a4f031ba5bb0ad5f79e003232
Chengyi Yang <ycysuk@gmail.com>
2021-03-31 15:29:50 +0800

    Add files via upload

---
+++ example/celery-4.4.7-py2.py3-none-any.whl_pip.sh
@@ -0,0 +1,8 @@
+# to install ./example/celery-4.4.7-py2.py3-none-any.whl, max_depth=-1...
+pip install \
+./example/celery-4.4.7_deps/pytz-2021.1-py2.py3-none-any.whl \
+./example/celery-4.4.7_deps/billiard-3.6.3.0-py3-none-any.whl \
+./example/celery-4.4.7_deps/kombu-4.6.11_deps/amqp-2.6.1_deps/vine-1.3.0-py2.py3-none-any.whl \
+./example/celery-4.4.7_deps/kombu-4.6.11_deps/amqp-2.6.1-py2.py3-none-any.whl \
+./example/celery-4.4.7_deps/kombu-4.6.11-py2.py3-none-any.whl \
+./example/celery-4.4.7-py2.py3-none-any.whl
